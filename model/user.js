4af00f552d0900c6f6e2932d561f4f85a2b3b3c0
robinmahto <robinmahto413@gmail.com>
2022-11-10 23:41:10 +0530

    designing user schema in model

--- model/user.js
+++ model/user.js
@@ -3,5 +3,24 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    
+    firstname : {
+        type: String,
+        required: true
+    },
+    lastname : {
+        type: String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true
+    },
+    password : {
+        type: String,
+        required: true
+    }
 })
+
+const userModel =  mongoose.model('user', userSchema)
+
+export default userModel;
